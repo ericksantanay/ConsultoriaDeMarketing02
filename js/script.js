49acb1b8ffce175733a623ae98bf22b58a2f11ad
Erick Silva <donizttierick@gmail.com>
2025-11-04 15:45:20 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+    let r = document.getElementById('burguer-menu')
 
+
+    // PARTE DO MENU
     function ClickMenu() {
 
     if (itens.style.display == 'block') {
        itens.style.display = 'none'
+       r.innerText = ('menu')
     }else {
        itens.style.display = 'block'
+       r.innerText = ('close')
     }
 }
 
